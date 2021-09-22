b8fbfc49916143fe1093502c06239e13e7d84d4d
jens <jenscollaertprive@hotmail.com>
2021-09-22 07:55:34 +0200

    update database

--- src/main/java/net/craftersland/ecobridge/database/MysqlSetup.java
+++ src/main/java/net/craftersland/ecobridge/database/MysqlSetup.java
@@ -1,21 +1,22 @@
 package net.craftersland.ecobridge.database;
 
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 
 import net.craftersland.ecobridge.Eco;
 import org.bukkit.Bukkit;
 
 public class MysqlSetup {
-	
-	private Connection conn = null;
-	private Eco eco;
-	
+
+	private static Connection connection;
+	private final Eco eco;
+	public String host;
+	public String database;
+	public String username;
+	public String password;
+	public int port;
+	public String ecoTable;
+
 	public MysqlSetup(Eco eco) {
 		this.eco = eco;
 		connectToDatabase();
@@ -23,71 +24,89 @@ public class MysqlSetup {
 		updateTables();
 		databaseMaintenanceTask();
 	}
-	
+
 	public void connectToDatabase() {
+
+		host = eco.getConfigHandler().getString("database.mysql.user");
+		port = eco.getConfigHandler().getInteger("database.mysql.port");
+		database = eco.getConfigHandler().getString("database.mysql.databaseName");
+		username = eco.getConfigHandler().getString("database.mysql.user");
+		password = eco.getConfigHandler().getString("database.mysql.password");
+		ecoTable = "MysqlEcoBridge";
+
 		Eco.log.info("Connecting to the database...");
+		//Load Drivers
 		try {
-       	 	//Load Drivers
-			Class.forName("com.mysql.cj.jdbc.Driver");
-            Properties properties = new Properties();
-            properties.setProperty("user", eco.getConfigHandler().getString("database.mysql.user"));
-            properties.setProperty("password", eco.getConfigHandler().getString("database.mysql.password"));
-            properties.setProperty("autoReconnect", "true");
-            properties.setProperty("verifyServerCertificate", "false");
-            properties.setProperty("useSSL", eco.getConfigHandler().getString("database.mysql.sslEnabled"));
-            properties.setProperty("requireSSL", eco.getConfigHandler().getString("database.mysql.sslEnabled"));
-            
-            //Connect to database
-            conn = DriverManager.getConnection("jdbc:mysql://" + eco.getConfigHandler().getString("database.mysql.host") + ":" + eco.getConfigHandler().getString("database.mysql.port") + "/" + eco.getConfigHandler().getString("database.mysql.databaseName"), properties);
-           
-          } catch (ClassNotFoundException e) {
-        	  Eco.log.severe("Could not locate drivers for mysql! Error: " + e.getMessage());
-            return;
-          } catch (SQLException e) {
-        	  Eco.log.severe("Could not connect to mysql database! Error: " + e.getMessage());
-            return;
-          }
+			synchronized (this) {
+				if (getConnection() != null && !getConnection().isClosed()) {
+					return;
+				}
+				// Database connection
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				Properties properties = new Properties();
+				properties.setProperty("autoReconnect", "true");
+				properties.setProperty("verifyServerCertificate", "false");
+				properties.setProperty("useSSL", eco.getConfigHandler().getString("database.mysql.sslEnabled"));
+				properties.setProperty("requireSSL", eco.getConfigHandler().getString("database.mysql.sslEnabled"));
+				setConnection(DriverManager.getConnection("jdbc:mysql://" + this.host + ":"
+						+ this.port + "/" + this.database, this.username, this.password + properties));
+				Statement statement = connection.createStatement();
+				statement.executeUpdate("CREATE TABLE IF NOT EXISTS " + ecoTable + "(id int(10) AUTO_INCREMENT, player_uuid varchar(50) NOT NULL UNIQUE, player_name varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL, money double(30,2) NOT NULL, sync_complete varchar(5) NOT NULL, last_seen varchar(30) NOT NULL, PRIMARY KEY(id))");
+
+			}
+
+		} catch (ClassNotFoundException e) {
+			Eco.log.severe("Could not locate drivers for mysql! Error: " + e.getMessage());
+			return;
+		} catch (SQLException e) {
+			Eco.log.severe("Could not connect to mysql database! Error: " + e.getMessage());
+			return;
+		}
 		Eco.log.info("Database connection successful!");
 	}
-	
+
 	public void setupDatabase() {
-		if (conn == null) return;
+		if (connection == null) return;
 		//Create tables if needed
 		PreparedStatement query = null;
-	      try {	        
-	        String data = "CREATE TABLE IF NOT EXISTS `" + eco.getConfigHandler().getString("database.mysql.dataTableName") + "` (id int(10) AUTO_INCREMENT, player_uuid varchar(50) NOT NULL UNIQUE, player_name varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL, money double(30,2) NOT NULL, sync_complete varchar(5) NOT NULL, last_seen varchar(30) NOT NULL, PRIMARY KEY(id));";
-	        query = conn.prepareStatement(data);
-	        query.execute();
-	      } catch (SQLException e) {
-	        e.printStackTrace();
-	        Eco.log.severe("Error creating tables! Error: " + e.getMessage());
-	      } finally {
-	    	  try {
-	    		  if (query != null) {
-	    			  query.close();
-	    		  }
-	    	  } catch (Exception e) {
-	    		  e.printStackTrace();
-	    	  }
-	      }
+		try {
+			String data = "CREATE TABLE IF NOT EXISTS `" + eco.getConfigHandler().getString("database.mysql.dataTableName") + "` (id int(10) AUTO_INCREMENT, player_uuid varchar(50) NOT NULL UNIQUE, player_name varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL, money double(30,2) NOT NULL, sync_complete varchar(5) NOT NULL, last_seen varchar(30) NOT NULL, PRIMARY KEY(id));";
+			query = connection.prepareStatement(data);
+			query.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			Eco.log.severe("Error creating tables! Error: " + e.getMessage());
+		} finally {
+			try {
+				if (query != null) {
+					query.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	public Connection getConnection() {
 		checkConnection();
-		return conn;
+		return connection;
+	}
+
+	public void setConnection(Connection connection) {
+		MysqlSetup.connection = connection;
 	}
 	
 	public void checkConnection() {
 		try {
-			if (conn == null) {
+			if (connection == null) {
 				Eco.log.warning("Connection failed. Reconnecting...");
 				reConnect();
 			}
-			if (!conn.isValid(3)) {
+			if (!connection.isValid(3)) {
 				Eco.log.warning("Connection is idle or terminated. Reconnecting...");
 				reConnect();
 			}
-			if (conn.isClosed()) {
+			if (connection.isClosed()) {
 				Eco.log.warning("Connection is closed. Reconnecting...");
 				reConnect();
 			}
@@ -96,14 +115,14 @@ public class MysqlSetup {
 		}
 	}
 	
-	public boolean reConnect() {
+	public void reConnect() {
 		try {            
-            long start = 0;
-			long end = 0;
+            long start;
+			long end;
 			
 		    start = System.currentTimeMillis();
 		    Eco.log.info("Attempting to establish a connection to the MySQL server!");
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             Properties properties = new Properties();
             properties.setProperty("user", eco.getConfigHandler().getString("database.mysql.user"));
             properties.setProperty("password", eco.getConfigHandler().getString("database.mysql.password"));
@@ -111,39 +130,37 @@ public class MysqlSetup {
             properties.setProperty("verifyServerCertificate", "false");
             properties.setProperty("useSSL", eco.getConfigHandler().getString("database.mysql.sslEnabled"));
             properties.setProperty("requireSSL", eco.getConfigHandler().getString("database.mysql.sslEnabled"));
-            conn = DriverManager.getConnection("jdbc:mysql://" + eco.getConfigHandler().getString("database.mysql.host") + ":" + eco.getConfigHandler().getString("database.mysql.port") + "/" + eco.getConfigHandler().getString("database.mysql.databaseName"), properties);
+            connection = DriverManager.getConnection("jdbc:mysql://" + eco.getConfigHandler().getString("database.mysql.host") + ":" + eco.getConfigHandler().getString("database.mysql.port") + "/" + eco.getConfigHandler().getString("database.mysql.databaseName"), properties);
 		    end = System.currentTimeMillis();
 		    Eco.log.info("Connection to MySQL server established in " + ((end - start)) + " ms!");
-            return true;
 		} catch (Exception e) {
 			Eco.log.severe("Error re-connecting to the database! Error: " + e.getMessage());
-			return false;
 		}
 	}
 	
 	public void closeConnection() {
 		try {
 			Eco.log.info("Closing database connection...");
-			conn.close();
-			conn = null;
+			connection.close();
+			connection = null;
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
 	
 	private void updateTables() {
-		if (conn != null) {
-			DatabaseMetaData md = null;
+		if (connection != null) {
+			DatabaseMetaData md;
 	    	ResultSet rs1 = null;
 	    	PreparedStatement query1 = null;
 	    	try {
-	    		md = conn.getMetaData();
+	    		md = connection.getMetaData();
 	    		rs1 = md.getColumns(null, null, eco.getConfigHandler().getString("database.mysql.dataTableName"), "sync_complete");
 	            if (rs1.next()) {
 			    	
 			    } else {
 			        String data = "ALTER TABLE `" + eco.getConfigHandler().getString("database.mysql.dataTableName") + "` ADD sync_complete varchar(5) NOT NULL DEFAULT 'true';";
-			        query1 = conn.prepareStatement(data);
+			        query1 = connection.prepareStatement(data);
 			        query1.execute();
 			    }
 	    	} catch (Exception e) {
@@ -165,37 +182,32 @@ public class MysqlSetup {
 	
 	private void databaseMaintenanceTask() {
 		if (eco.getConfigHandler().getBoolean("database.removeOldAccounts.enabled")) {
-			Bukkit.getScheduler().runTaskLaterAsynchronously(eco, new Runnable() {
-
-				@Override
-				public void run() {
-					if (conn != null) {
-						long inactivityDays = Long.parseLong(eco.getConfigHandler().getString("database.removeOldAccounts.inactivity"));
-						long inactivityMils = inactivityDays * 24 * 60 * 60 * 1000;
-						long curentTime = System.currentTimeMillis();
-						long inactiveTime = curentTime - inactivityMils;
-						Eco.log.info("Database maintenance task started...");
-						PreparedStatement preparedStatement = null;
+			Bukkit.getScheduler().runTaskLaterAsynchronously(eco, () -> {
+				if (connection != null) {
+					long inactivityDays = Long.parseLong(eco.getConfigHandler().getString("database.removeOldAccounts.inactivity"));
+					long inactivityMils = inactivityDays * 24 * 60 * 60 * 1000;
+					long currentTime = System.currentTimeMillis();
+					long inactiveTime = currentTime - inactivityMils;
+					Eco.log.info("Database maintenance task started...");
+					PreparedStatement preparedStatement = null;
+					try {
+						String sql = "DELETE FROM `" + eco.getConfigHandler().getString("database.mysql.dataTableName") + "` WHERE `last_seen` < ?";
+						preparedStatement = connection.prepareStatement(sql);
+						preparedStatement.setString(1, String.valueOf(inactiveTime));
+						preparedStatement.execute();
+					} catch (Exception e) {
+						e.printStackTrace();
+					} finally {
 						try {
-							String sql = "DELETE FROM `" + eco.getConfigHandler().getString("database.mysql.dataTableName") + "` WHERE `last_seen` < ?";
-							preparedStatement = conn.prepareStatement(sql);
-							preparedStatement.setString(1, String.valueOf(inactiveTime));
-							preparedStatement.execute();
+							if (preparedStatement != null) {
+								preparedStatement.close();
+							}
 						} catch (Exception e) {
 							e.printStackTrace();
-						} finally {
-							try {
-								if (preparedStatement != null) {
-									preparedStatement.close();
-								}
-							} catch (Exception e) {
-								e.printStackTrace();
-							}
 						}
-						Eco.log.info("Database maintenance complete!");
 					}
+					Eco.log.info("Database maintenance complete!");
 				}
-				
 			}, 100 * 20L);
 		}
 	}
