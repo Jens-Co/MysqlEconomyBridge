f243ccb105d3d55238735da4f72b1f3dacdb98c3
Jens Collaert <jenscollaertprive@hotmail.com>
2021-09-22 20:21:52 +0200

    changed mysql driver

--- src/main/java/net/craftersland/ecobridge/database/MysqlSetup.java
+++ src/main/java/net/craftersland/ecobridge/database/MysqlSetup.java
@@ -1,7 +1,6 @@
 package net.craftersland.ecobridge.database;
 
 import java.sql.*;
-import java.util.Properties;
 
 import net.craftersland.ecobridge.Eco;
 import org.bukkit.Bukkit;
@@ -10,46 +9,31 @@ public class MysqlSetup {
 
 	private static Connection connection;
 	private final Eco eco;
-	public String host;
-	public String database;
-	public String username;
-	public String password;
-	public int port;
-	public String ecoTable;
 
 	public MysqlSetup(Eco eco) {
 		this.eco = eco;
 		connectToDatabase();
-		setupDatabase();
 		updateTables();
 		databaseMaintenanceTask();
 	}
 
 	public void connectToDatabase() {
 
-		host = eco.getConfigHandler().getString("database.mysql.user");
-		port = eco.getConfigHandler().getInteger("database.mysql.port");
-		database = eco.getConfigHandler().getString("database.mysql.databaseName");
-		username = eco.getConfigHandler().getString("database.mysql.user");
-		password = eco.getConfigHandler().getString("database.mysql.password");
-		ecoTable = "MysqlEcoBridge";
+		String host = eco.getConfigHandler().getString("database.mysql.host");
+		int port = eco.getConfigHandler().getInteger("database.mysql.port");
+		String database = eco.getConfigHandler().getString("database.mysql.databaseName");
+		String username = eco.getConfigHandler().getString("database.mysql.user");
+		String password = eco.getConfigHandler().getString("database.mysql.password");
+		String ecoTable = eco.getConfigHandler().getString("database.mysql.dataTableName");
 
 		Eco.log.info("Connecting to the database...");
 		//Load Drivers
 		try {
 			synchronized (this) {
-				if (getConnection() != null && !getConnection().isClosed()) {
-					return;
-				}
 				// Database connection
 				Class.forName("com.mysql.cj.jdbc.Driver");
-				Properties properties = new Properties();
-				properties.setProperty("autoReconnect", "true");
-				properties.setProperty("verifyServerCertificate", "false");
-				properties.setProperty("useSSL", eco.getConfigHandler().getString("database.mysql.sslEnabled"));
-				properties.setProperty("requireSSL", eco.getConfigHandler().getString("database.mysql.sslEnabled"));
-				setConnection(DriverManager.getConnection("jdbc:mysql://" + this.host + ":"
-						+ this.port + "/" + this.database, this.username, this.password + properties));
+				setConnection(DriverManager.getConnection("jdbc:mysql://" + host + ":"
+						+ port + "/" + database, username, password));
 				Statement statement = connection.createStatement();
 				statement.executeUpdate("CREATE TABLE IF NOT EXISTS " + ecoTable + "(id int(10) AUTO_INCREMENT, player_uuid varchar(50) NOT NULL UNIQUE, player_name varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL, money double(30,2) NOT NULL, sync_complete varchar(5) NOT NULL, last_seen varchar(30) NOT NULL, PRIMARY KEY(id))");
 
@@ -63,28 +47,6 @@ public class MysqlSetup {
 			return;
 		}
 		Eco.log.info("Database connection successful!");
-	}
-
-	public void setupDatabase() {
-		if (connection == null) return;
-		//Create tables if needed
-		PreparedStatement query = null;
-		try {
-			String data = "CREATE TABLE IF NOT EXISTS `" + eco.getConfigHandler().getString("database.mysql.dataTableName") + "` (id int(10) AUTO_INCREMENT, player_uuid varchar(50) NOT NULL UNIQUE, player_name varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL, money double(30,2) NOT NULL, sync_complete varchar(5) NOT NULL, last_seen varchar(30) NOT NULL, PRIMARY KEY(id));";
-			query = connection.prepareStatement(data);
-			query.execute();
-		} catch (SQLException e) {
-			e.printStackTrace();
-			Eco.log.severe("Error creating tables! Error: " + e.getMessage());
-		} finally {
-			try {
-				if (query != null) {
-					query.close();
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
 	}
 	
 	public Connection getConnection() {
@@ -123,14 +85,7 @@ public class MysqlSetup {
 		    start = System.currentTimeMillis();
 		    Eco.log.info("Attempting to establish a connection to the MySQL server!");
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Properties properties = new Properties();
-            properties.setProperty("user", eco.getConfigHandler().getString("database.mysql.user"));
-            properties.setProperty("password", eco.getConfigHandler().getString("database.mysql.password"));
-            properties.setProperty("autoReconnect", "true");
-            properties.setProperty("verifyServerCertificate", "false");
-            properties.setProperty("useSSL", eco.getConfigHandler().getString("database.mysql.sslEnabled"));
-            properties.setProperty("requireSSL", eco.getConfigHandler().getString("database.mysql.sslEnabled"));
-            connection = DriverManager.getConnection("jdbc:mysql://" + eco.getConfigHandler().getString("database.mysql.host") + ":" + eco.getConfigHandler().getString("database.mysql.port") + "/" + eco.getConfigHandler().getString("database.mysql.databaseName"), properties);
+            connection = DriverManager.getConnection("jdbc:mysql://" + eco.getConfigHandler().getString("database.mysql.host") + ":" + eco.getConfigHandler().getString("database.mysql.port") + "/" + eco.getConfigHandler().getString("database.mysql.databaseName"));
 		    end = System.currentTimeMillis();
 		    Eco.log.info("Connection to MySQL server established in " + ((end - start)) + " ms!");
 		} catch (Exception e) {
